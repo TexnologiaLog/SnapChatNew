e3e8cf49fc575293fc8c74444c69703257a5b54f
Stoikosn <stoikosn@gmail.com>
2015-11-20 18:13:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NIKOS
+ */
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="index_css_page.css" rel="stylesheet" type="text/css" />	
+<title>index</title>
+</head>
+<body>
+ <h1>Welcome</h1>
+ <p>Have you got an account already?</p>
+
+     <ul>
+        <li class="child"><a href="Register.php">Create Account</a></li>
+        <li><a href="Login.php">Login to Account</a></li>
+     </ul>
+
+</body>
+</html>
