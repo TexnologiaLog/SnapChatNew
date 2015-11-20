fb96a65a8b2ac78afc24f7c7e1c91dc55ee11564
Stoikosn <stoikosn@gmail.com>
2015-11-20 18:12:00 +0200

    Create User.php

---
+++ User.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NIKOS
+ */
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="profile_css.css" rel="stylesheet" type="text/css" />	
+<title>Profile</title>
+
+</head>
+<body>
+   <nav>
+       <ul>
+	      <li class="child"><a href="#">My Info</a>
+		     <ul class="sub">
+		         <li><a href="#">Show</a></li>
+		     </ul>
+		     <ul class="sub">
+		         <li><a href="#">Edit</a></li>
+		     </ul>			 
+		  </li>
+	      <li class="child1"><a href="#">My Friends</a>
+		  	 <ul class="sub">
+		         <li><a href="#">Show All</a></li>
+		     </ul>
+		     <ul class="sub">
+		         <li><a href="#">Search</a></li>
+		     </ul>	
+		  </li>
+	      <li class="child1"><a href="#">My Snaps</a>
+		  	 <ul class="sub">
+		         <li><a href="#">Show All</a></li>
+		     </ul>
+		     <ul class="sub">
+		         <li><a href="#">Unread</a></li>
+		     </ul>
+		  </li>
+		  <li class="child1"><a href="#">My Photos</a>
+		     <ul class="sub">
+		         <li><a href="#">Show All</a></li>
+		     </ul>
+		     <ul class="sub">
+		         <li><a href="#">Edit</a></li>
+		     </ul>
+		  </li>
+		  <li class="child1"><a href="#">My Videos</a>
+		     <ul class="sub">
+		         <li><a href="#">Show All</a></li>
+		     </ul>
+		     <ul class="sub">
+		         <li><a href="#">Edit</a></li>
+		     </ul>
+		  </li>
+		  <li class="child2"><a href="Logout.php">Logout</a>		     
+		  </li>		  
+	   </ul>
+   </nav>
+</body>
+</html>
